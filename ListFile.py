12df8013eef287a5195046e97b12730e3e29948c
baijuminglu <wangxingwei0624@gmail.com>
2018-08-15 11:40:28 +0800

    add test for  ListFile

--- ListFile.py
+++ ListFile.py
@@ -6,6 +6,7 @@ class ListFile:
 
     def printfile(self,file_json):
         self.stack.append(file_json[0]["name"])
+        print(self.stack)
         for i in range(1,len(file_json)):
             if isinstance(file_json[i], list):
                  self.printfile(file_json[i])
@@ -15,7 +16,6 @@ class ListFile:
                     self.Dict["/".join(self.stack)]=file_json[i]["asize"]
                 else:
                     self.Dict["/".join(self.stack)]=0
-                print(self.Dict)
                 self.stack.pop()
         self.stack.pop()
 
@@ -31,5 +31,89 @@ class ListFile:
 
 
 if __name__ == "__main__":
+    a = [
+        1, 
+        1, 
+        {
+            "progname": "ncdu", 
+            "progver": "1.13", 
+            "timestamp": 1534244357
+        }, 
+        [
+            {
+                "name": "/usr/local/src/test", 
+                "asize": 4096, 
+                "dsize": 4096, 
+                "dev": 64769, 
+                "ino": 1328152
+            }, 
+            [
+                {
+                    "name": "t1", 
+                    "asize": 4096, 
+                    "dsize": 4096, 
+                    "ino": 1448563
+                }, 
+                [
+                    {
+                        "name": "s1", 
+                        "asize": 4096, 
+                        "dsize": 4096, 
+                        "ino": 1448567
+                    }, 
+                    [
+                        {
+                            "name": "n1", 
+                            "asize": 4096, 
+                            "dsize": 4096, 
+                            "ino": 1448569
+                        }, 
+                        {
+                            "name": "2.tt", 
+                            "ino": 1448570
+                        }, 
+                        {
+                            "name": "3.tt", 
+                            "ino": 1448571
+                        }
+                    ], 
+                    {
+                        "name": "1.txt", 
+                        "ino": 1448568
+                    }
+                ]
+            ], 
+            [
+                {
+                    "name": "t3", 
+                    "asize": 4096, 
+                    "dsize": 4096, 
+                    "ino": 1448565
+                }
+            ], 
+            [
+                {
+                    "name": "t2", 
+                    "asize": 4096, 
+                    "dsize": 4096, 
+                    "ino": 1448564
+                }
+            ], 
+            [
+                {
+                    "name": "t4", 
+                    "asize": 4096, 
+                    "dsize": 4096, 
+                    "ino": 1448566
+                }
+            ], 
+            {
+                "name": "1.txt", 
+                "asize": 10485760, 
+                "dsize": 10485760, 
+                "ino": 1328153
+            }
+        ]
+    ]
     dir_a = ListFile(a) 
     dir_a.getlargefile(4)  
